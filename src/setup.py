a6463cf9d317c1ef663c9b5a7c6d5ec4a771de10
bojoluis21798 <trinity21798@gmail.com>
2018-07-24 14:13:01 +0800

    Load filesname to array

--- src/setup.py
+++ src/setup.py
@@ -3,5 +3,9 @@ from os import walk
 def modelsLoad():
     """ Load all models in ./data/stage to memory and get all attributes of object from metadata.csv"""
 
+    filenames = []
     for __, __, files in walk("./data/stage/"):
-        print (files)
+        filenames.extend(files)
+
+    # print (filenames)
+
