cd0676942960a2e48f76ddd289d90abfaa9d43e5
bojoluis21798 <trinity21798@gmail.com>
2018-07-24 16:05:30 +0800

    Display menu to loaded models

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,22 @@
-from os import listdir
+from os import listdir, system
 from os.path import isfile, join
 from src.model import Model
+
+def menu(models):
+    """ Menu to display models and their attributes """
+    while(True):
+        system('cls')
+        print ("Loaded models\n")
+
+        for i in range(len(models)):
+            print ("Model Number: "+str(i))
+            print ("Name: "+models[i].name)
+            print ("========================")
+
+        modelNum = input('Enter model number (-1 to exit): ')
+
+        if int(modelNum) == -1:
+            break
 
 def modelsLoad():
     """ Load all models in ./data/stage to memory and get all attributes of object from metadata.csv """
