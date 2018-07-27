31a514e5a1223eb1c4f9151d841b27745203d536
Carlo Janea <janeacarlo@gmail.com>
2018-07-27 09:43:02 +0800

    added fitness evaluation function

--- src/locustParticle.py
+++ src/locustParticle.py
@@ -1,5 +1,3 @@
-from src.optimization import objectiveFunc
-
 class LocustParticle:
     def __init__():
         self.state = 0          # 0 - solitary, 1 - gregarious
@@ -8,3 +6,11 @@ class LocustParticle:
         self.err_best_i=-1      # best error individual
         self.err_i=-1           # error individual
 
+    # evaluate this particle's current fitness
+    def evaluate(self, costFunc, box):
+        self.err_i=costFunc(self.position_i, box)
+        
+        # check to see if the current position is an individual best
+        if self.err_i < self.err_best_i or self.err_best_i == -1:
+            self.pos_best_i = self.position_i
+            self.err_best_i = self.err_i
