d1cf36a6aafdd29109f51fead328ee058da2e39d
Carlo Janea <janeacarlo@gmail.com>
2018-07-28 09:52:18 +0800

    added updateVelocity function

--- src/locustParticle.py
+++ src/locustParticle.py
@@ -1,5 +1,8 @@
+import random
+
+# doesnt conform to a 3D box yet, just 1d array pa
 class LocustParticle:
-    def __init__():
+    def __init__(self, bounds, num_dimensions):
         self.state = 0              # 0 - solitary, 1 - gregarious
         self.position_i = []        # particle position
         self.velocity_i = []        # particle current velocity
@@ -10,6 +13,10 @@ class LocustParticle:
         self.c2 = 2                 # social parameter constant
         self.w = 0.5                # inertia constant
 
+        for i in range(0,num_dimensions):
+            self.velocity_i.append(random.uniform(-1,1)) #### why (-1,1)
+            self.position_i.append(bounds[i])
+
     # evaluate this particle's current fitness
     def evaluate(self, costFunc, box):
         self.err_i=costFunc(self.position_i, box)
@@ -18,3 +25,13 @@ class LocustParticle:
         if self.err_i < self.err_best_i or self.err_best_i == -1:
             self.pos_best_i = self.position_i
             self.err_best_i = self.err_i
+
+    # update particles velocity
+    def updateVelocity(self, pos_best_g, num_dimensions):
+        for i in range(0, num_dimensions):
+            r1 = random.random()
+            r2 = random.random()
+
+            vel_cognitive = self.c1 * r1 * ( self.pos_best_i[i] - self.position_i[i] )
+            vel_social = self.c2 * r2 * ( pos_best_g[i] - self.position_i[i] )
+            self.velocity_i[i] = self.w * self.velocity_i[i] + vel_cognitive +vel_social 
