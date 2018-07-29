60879f180cde1a3652d561e5bd57117d95371354
Carlo Janea <janeacarlo@gmail.com>
2018-07-29 14:02:00 +0800

    added variable comments

--- src/box.py
+++ src/box.py
@@ -8,8 +8,8 @@ class Box:
         self.width  = width
         self.height = height
         self.totalVolume = length * width * height
-        self.numObjects = 0
-        self.totalObjectVolume = 0;
+        self.numObjects = 0        # number of objects inserted in box
+        self.totalObjectVolume = 0 # gives the total occupied volume of the objects inside box
 
         #3dbox with l x w x h mm dimension with datatype 'u1' = 8bit unsigned int (0 to 160)
         self.boxgrid = np.zeros( ( int(length * 1000), int(width * 1000), int(height * 1000) ), dtype = 'u1')
