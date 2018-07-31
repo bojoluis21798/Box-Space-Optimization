6ab87f318b289a3553e9b2b844ffdec4c6db01c2
Carlo Janea <janeacarlo@gmail.com>
2018-07-31 10:25:44 +0800

    added space availability for particle position

--- src/locustParticle.py
+++ src/locustParticle.py
@@ -2,7 +2,8 @@ import random
 
 # doesnt conform to a 3D box yet, just 1d array pa
 class LocustParticle:
-    def __init__(self, bounds, num_dimensions):
+
+    def __init__(self, bounds, num_dimensions, item):
         self.state = 0              # 0 - solitary, 1 - gregarious
         self.position_i = []        # particle position
         self.velocity_i = []        # particle current velocity
@@ -12,10 +13,11 @@ class LocustParticle:
         self.c1 = 1                 # cognitive parameter constant
         self.c2 = 2                 # social parameter constant
         self.w = 0.5                # inertia constant
+        self.item = item            # the item it currently is looking for an optimal space
 
         for i in range(0,num_dimensions):
             self.velocity_i.append(random.uniform(-1,1)) #### why (-1,1)
-            self.position_i.append(bounds[i])
+            self.position_i.append(bounds[i]) #check if space is available
 
     # evaluate this particle's current fitness
     def evaluate(self, costFunc, box):
@@ -39,7 +41,7 @@ class LocustParticle:
     # update particles position
     def updatePosition(self, bounds, num_dimensions):
         for i in range(0, num_dimensions):
-            self.position_i[i] = self.position_i[i] + self.velocity_i[i]
+            self.position_i[i] = self.position_i[i] + self.velocity_i[i] #check if space is available
 
             # adjust maximum position if necessary
             if self.position_i[i] > bounds[i][1]:
@@ -48,4 +50,25 @@ class LocustParticle:
             # adjust minimum position if neseccary
             if self.position_i[i] < bounds[i][0]:
                 self.position_i[i] = bounds[i][0]
+
+    # @params item = self.item
+    # @params position = given particle position
+    # @params box = box space being  worked on (probably bounds)
+    # this function is used assuming a particle position is the beginning of the space
+    def checkSpaceAvailability(item, position, box):
+        #get item dimensions
+        #compare with position if available in box
+        ret = False
+        posX = position[0]
+        posY = position[1]
+        posZ = position[2]
+        #check if not over the box dimensions
+        if posX + item.dimX < box.convertMeterToMilli(box.length) and posY + item.dimY < convertMeterToMilli(box.width)  and posZ < convertMeterToMilli(box.height) :
+            limit = box[posX:posX + item.dimX, posY:posY + item.dimY, posZ:posZ + item.dimZ]
+            #check if all in splice is 0, otherwise return false
+            if np.count_nonzero(limit) == 0:
+                ret = True
+        
+        return ret
+        
 
