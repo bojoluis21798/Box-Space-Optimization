70a243fbd304af85e84938b9bc9790c20af4c370
bojoluis21798 <trinity21798@gmail.com>
2018-08-08 20:05:04 +0800

    Show model info

--- src/setup.py
+++ src/setup.py
@@ -19,10 +19,33 @@ def menu(models):
         # hide menu items
         menu.hide()
 
-        # display 3d model
         i = 0
-        currentModel = loader.loadModel(models[2].filename)
-        currentModel.setScale(models[2].unit*15)
+
+        # display model info
+        info = aspect2d.attachNewNode("Info")
+
+        textObject = OnscreenText(text = "id: "+models[i].id, pos = (-1.2,0.9),
+        scale = 0.06,fg=(1,0.5,0.5,1), parent = info, align=TextNode.ALeft,mayChange=0)
+        textObject = OnscreenText(text = "x-dimension: "+str(models[i].dimX), pos = (-1.2,0.8),
+        scale = 0.06,fg=(1,0.5,0.5,1), parent = info, align=TextNode.ALeft,mayChange=0)
+        textObject = OnscreenText(text = "y-dimension: "+str(models[i].dimY), pos = (-1.2,0.7),
+        scale = 0.06,fg=(1,0.5,0.5,1), parent = info, align=TextNode.ALeft,mayChange=0)
+        textObject = OnscreenText(text = "z-dimension: "+str(models[i].dimZ), pos = (-1.2,0.6),
+        scale = 0.06,fg=(1,0.5,0.5,1), parent = info, align=TextNode.ALeft,mayChange=0)
+        textObject = OnscreenText(text = "Surface Volume: "+str(models[i].surfaceVolume), pos = (1.2,0.9),
+        scale = 0.06,fg=(1,0.5,0.5,1), parent = info, align=TextNode.ARight,mayChange=0)
+        textObject = OnscreenText(text = "Solid Volume: "+str(models[i].solidVolume), pos = (1.2,0.8),
+        scale = 0.06,fg=(1,0.5,0.5,1), parent = info, align=TextNode.ARight,mayChange=0)
+        textObject = OnscreenText(text = "Support Surface Area: "+str(models[i].supportSurfaceArea), pos = (1.2,0.7),
+        scale = 0.06,fg=(1,0.5,0.5,1), parent = info, align=TextNode.ARight,mayChange=0)
+        textObject = OnscreenText(text = "Weight: "+str(models[i].weight), pos = (1.2,0.6),
+        scale = 0.06,fg=(1,0.5,0.5,1), parent = info, align=TextNode.ARight,mayChange=0)
+        textObject = OnscreenText(text = "Static Friction: "+str(models[i].staticFriction), pos = (1.2,0.5),
+        scale = 0.06,fg=(1,0.5,0.5,1), parent = info, align=TextNode.ARight,mayChange=0)
+
+        # display 3d model
+        currentModel = loader.loadModel(models[i].filename)
+        currentModel.setScale(models[i].unit*15)
         currentModel.setPos(-0.1,2,-0.2)
 
         currentModel.reparentTo(render)
