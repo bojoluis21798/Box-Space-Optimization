c83f7291698d54c043c9bf6c619ad9cd0555e1a0
bojoluis21798 <trinity21798@gmail.com>
2018-08-08 22:22:32 +0800

    Traverse to all loaded models

--- src/setup.py
+++ src/setup.py
@@ -13,39 +13,52 @@ def menu(models):
 
     menu = aspect2d.attachNewNode("Menu")
 
-    def displayModels():
+    i = 0
+
+    def displayModels(idx = 0):
         """ Show models models """
 
         # hide menu items
         menu.hide()
 
-        i = 0
+        info = aspect2d.attachNewNode("Info")
+        currentModel = loader.loadModel(models[idx].filename)
+
+        def increment():
+            nonlocal i
+            nonlocal models
+            i = (i+1)%len(models)
+            info.removeNode()
+            currentModel.removeNode()
+            displayModels(i)
 
         # display model info
-        info = aspect2d.attachNewNode("Info")
 
-        textObject = OnscreenText(text = "id: "+models[i].id, pos = (-1.2,0.9),
+
+        textObject = OnscreenText(text = "id: "+models[idx].id, pos = (-1.2,0.9),
         scale = 0.06,fg=(1,0.5,0.5,1), parent = info, align=TextNode.ALeft,mayChange=0)
-        textObject = OnscreenText(text = "x-dimension: "+str(models[i].dimX), pos = (-1.2,0.8),
+        textObject = OnscreenText(text = "x-dimension: "+str(models[idx].dimX), pos = (-1.2,0.8),
         scale = 0.06,fg=(1,0.5,0.5,1), parent = info, align=TextNode.ALeft,mayChange=0)
-        textObject = OnscreenText(text = "y-dimension: "+str(models[i].dimY), pos = (-1.2,0.7),
+        textObject = OnscreenText(text = "y-dimension: "+str(models[idx].dimY), pos = (-1.2,0.7),
         scale = 0.06,fg=(1,0.5,0.5,1), parent = info, align=TextNode.ALeft,mayChange=0)
-        textObject = OnscreenText(text = "z-dimension: "+str(models[i].dimZ), pos = (-1.2,0.6),
+        textObject = OnscreenText(text = "z-dimension: "+str(models[idx].dimZ), pos = (-1.2,0.6),
         scale = 0.06,fg=(1,0.5,0.5,1), parent = info, align=TextNode.ALeft,mayChange=0)
-        textObject = OnscreenText(text = "Surface Volume: "+str(models[i].surfaceVolume), pos = (1.2,0.9),
+        textObject = OnscreenText(text = "Surface Volume: "+str(models[idx].surfaceVolume), pos = (1.2,0.9),
         scale = 0.06,fg=(1,0.5,0.5,1), parent = info, align=TextNode.ARight,mayChange=0)
-        textObject = OnscreenText(text = "Solid Volume: "+str(models[i].solidVolume), pos = (1.2,0.8),
+        textObject = OnscreenText(text = "Solid Volume: "+str(models[idx].solidVolume), pos = (1.2,0.8),
         scale = 0.06,fg=(1,0.5,0.5,1), parent = info, align=TextNode.ARight,mayChange=0)
-        textObject = OnscreenText(text = "Support Surface Area: "+str(models[i].supportSurfaceArea), pos = (1.2,0.7),
+        textObject = OnscreenText(text = "Support Surface Area: "+str(models[idx].supportSurfaceArea), pos = (1.2,0.7),
         scale = 0.06,fg=(1,0.5,0.5,1), parent = info, align=TextNode.ARight,mayChange=0)
-        textObject = OnscreenText(text = "Weight: "+str(models[i].weight), pos = (1.2,0.6),
+        textObject = OnscreenText(text = "Weight: "+str(models[idx].weight), pos = (1.2,0.6),
         scale = 0.06,fg=(1,0.5,0.5,1), parent = info, align=TextNode.ARight,mayChange=0)
-        textObject = OnscreenText(text = "Static Friction: "+str(models[i].staticFriction), pos = (1.2,0.5),
+        textObject = OnscreenText(text = "Static Friction: "+str(models[idx].staticFriction), pos = (1.2,0.5),
         scale = 0.06,fg=(1,0.5,0.5,1), parent = info, align=TextNode.ARight,mayChange=0)
+
+        nextButton = DirectButton(text = "Next",
+        pos=(1.2,0,-0.9), parent=info, scale=.05, command = increment)
 
         # display 3d model
-        currentModel = loader.loadModel(models[i].filename)
-        currentModel.setScale(models[i].unit*15)
+        currentModel.setScale(models[idx].unit*15)
         currentModel.setPos(-0.1,2,-0.2)
 
         currentModel.reparentTo(render)
