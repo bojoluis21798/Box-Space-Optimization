ac319c8ef35c302dbb131eca0ec8bdd264a91be4
Carlo Janea <janeacarlo@gmail.com>
2018-08-30 18:36:00 +0800

    added scaled variables for box

--- src/box.py
+++ src/box.py
@@ -5,10 +5,20 @@ class Box:
     #maintained meters for attributes, but not in grid system which is in milli
     def __init__(self, length, width, height):
         self.convertionFactor = 25.4                                        # 1 inch == 25.4 mm
+        self.scaleFactor = 2
+
+        # for display
         self.length = int( length * self.convertionFactor )                 # convert inch
         self.width  = int( width * self.convertionFactor  )                 # convert inch
         self.height = int( height * self.convertionFactor )                 # convert inch
         self.totalVolume = int( self.length * self.width * self.height )
+
+        # for algorithm
+        self.scaledLength = int( self.length / self.scaleFactor)
+        self.scaledWidth  = int( self.width / self.scaleFactor)
+        self.scaledHeight = int( self.height / self.scaleFactor)
+        self.scaledTotalVolume = int(self.scaledLength * self.scaledWidth * self.scaledHeight)
+        
         self.numObjects = 0                                 # number of objects inserted in box
         self.totalObjectVolume = 0                          # gives the total occupied volume of the objects inside box
 
